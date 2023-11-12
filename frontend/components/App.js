75c2b980f18681c5ecf85ad2d2b2829efa605e60
DevCodeX1 <countryboya20@gmail.com>
2023-11-12 10:46:49 -0600

    Finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -21,17 +21,17 @@ function App() {
         console.log(err.message)
       })
     }
-    // fetchPhoto()
+    fetchPhoto()
     
-     setApod ({
-          "date": "2023-11-03",
-          "explanation": "That bright beacon you've seen rising in the east just after sunset is Jupiter. Climbing high in midnight skies, our Solar System's ruling gas giant was at its 2023 opposition, opposite the Sun in planet Earth's sky, on November 2. But only a few days earlier, on October 28, the Moon was at its own opposition. Then both Full Moon and Jupiter could share this telephoto field of view. The celestial scene is composed from two exposures, one long and one short, blended to record bright planet and ev...",
-          "hdurl": "https://apod.nasa.gov/apod/image/2311/_GHR7338_3_firma_picc.jpg",
-          "media_type": "image",
-          "service_version": "v1",
-          "title": "Jupiter by Moonlight",
-          "url": "https://apod.nasa.gov/apod/image/2311/_GHR7338_3_firma_picc1024.jpg"
-        })
+    //  setApod ({
+    //       "date": "2023-11-03",
+    //       "explanation": "That bright beacon you've seen rising in the east just after sunset is Jupiter. Climbing high in midnight skies, our Solar System's ruling gas giant was at its 2023 opposition, opposite the Sun in planet Earth's sky, on November 2. But only a few days earlier, on October 28, the Moon was at its own opposition. Then both Full Moon and Jupiter could share this telephoto field of view. The celestial scene is composed from two exposures, one long and one short, blended to record bright planet and ev...",
+    //       "hdurl": "https://apod.nasa.gov/apod/image/2311/_GHR7338_3_firma_picc.jpg",
+    //       "media_type": "image",
+    //       "service_version": "v1",
+    //       "title": "Jupiter by Moonlight",
+    //       "url": "https://apod.nasa.gov/apod/image/2311/_GHR7338_3_firma_picc1024.jpg"
+    //     })
        
   }, [])
 
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,11 +1,20 @@
 import React from "react"
+import styled from "styled-components"
 
+const StyledFigure = styled.figure`
+max-width: 60%;
+
+figcaption{
+  font-style: italic;
+}
+
+`
 
 export default function Figure({imageURL, caption}){
     return(
-      <figure>
+      <StyledFigure color="red">
         <img src={imageURL}/>
         <figcaption>Awsome picture taken on {caption}</figcaption>
-      </figure>
+      </StyledFigure>
     )
   }
